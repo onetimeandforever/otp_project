254be74e59a16753688026e8b019761002f4ec42
John Truong <52351958+onetimeandforever@users.noreply.github.com>
2021-12-27 09:31:57 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
+class App extends React.Component{
+  handleChange = (e) =>{
+    const{name, value} = e.target
+    this.setState(
+      {
+        [name]:value
+      }
+    )
+  }
+
+  configureCaptcha = ()=>{
+        const auth = getAuth();
+    window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+      'size': 'invisible',
+      'callback': (response) => {
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+        this.onSignInSubmit();
+        console.log("Recaptcha verified")
+  }
+}, auth);
+  }
+  onSignInSubmit = (e) => {
+    e.preventDefault()
+    this.configureCaptcha()
+    const phoneNumber = "+1" + this.state.mobile
+    const appVerifier = window.recaptchaVerifier;
+    console.log(phoneNumber)
+    
+    const auth = getAuth();
+    this.signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+        .then((confirmationResult) => {
+          // SMS sent. Prompt user to type the code from the message, then sign the
+          // user in with confirmationResult.confirm(code).
+          window.confirmationResult = confirmationResult;
+          console.log("OTP has been sent")
+          // ...
+        }).catch((error) => {
+          // Error; SMS not sent
+          // ...
+          console.log("SMS not sent")
+        });
+
+  }
+  onSubmitOTP = (e) => {
+    e.preventDefault()
+    const code = this.state.onSubmitOTP
+    console.log(code)
+    window.confirmationResult.confirm(code).then((result) => {
+      // User signed in successfully.
+      const user = result.user;
+      console.log(JSON.stringify(user))
+      alert("User is verified")
+      // ...
+    }).catch((error) => {
+      // User couldn't sign in (bad verification code?)
+      // ...
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>
+          Login Form
+        </h2>
+        <form onSubmit={this.onSignInSubmit}>
+          <div id="sign-in-button"></div>
+          <input type="number" name="mobile" placeholder="Mobile number" require onChane={this.handleChange}/>
+          <button type="check">Check</button>
+        </form>
+
+        <form onSubmit ={this.onSubmitOTP}>
+          <input type="number" name="OTP" placeholder="Acess number" require onChange={this.handleChange}/>
+        </form>
+        <form>
+          <button type="submit">Submit</button>
+        </form>
+        
+      </div>
+    )
+  }
+}
+export default App;
---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+const firebaseConfig = {
+    apiKey: "AIzaSyBS2DlDIfikKsndYn2st7ZIpgINNjjkPNo",
+    authDomain: "otp-project-4bf49.firebaseapp.com",
+    projectId: "otp-project-4bf49",
+    storageBucket: "otp-project-4bf49.appspot.com",
+    messagingSenderId: "1031054061827",
+    appId: "1:1031054061827:web:393acdedda199a27703936"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export default firebase
